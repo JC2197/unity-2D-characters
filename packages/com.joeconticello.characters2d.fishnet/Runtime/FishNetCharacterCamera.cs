b3efb076e2fbd23ea69fc9a5a62798548d2dc48b
JC2197 <JoeConti21@gmail.com>
2026-08-11 12:06:02 -0400

    FishNetCharacterCamera should work

--- packages/com.joeconticello.characters2d.fishnet/Runtime/FishNetCharacterCamera.cs
+++ packages/com.joeconticello.characters2d.fishnet/Runtime/FishNetCharacterCamera.cs
@@ -9,22 +9,56 @@ namespace JoeConticello.Characters2D.FishNet
 #if FISHNET
     // This script will be a NetworkBehaviour so that we can use the 
     // OnStartClient override.
-    public class FishnetPlayerCamera : NetworkBehaviour
+    public class FishNetCharacterCamera : NetworkBehaviour
     {
-        [SerializeField] private Camera _cameraPrefab;
         [SerializeField] private Transform _cameraHolder;
+        [SerializeField] private Color _backgroundColor = Color.black;
+        [SerializeField] private float _orthographicSize = 5f;
 
-        // This method will run on the client once this object is spawned.
+        private Camera _activeCamera;
+
         public override void OnStartClient()
+        => TryAttachCamera();
+
+        public override void OnOwnershipClient(NetworkConnection prevOwner)
+        => TryAttachCamera();
+
+        private void TryAttachCamera()
         {
-            // Since this will run on all clients that this object spawns for
-            // we need to only instantiate the camera for the object we own.
-            if (IsOwner)
-                Instantiate(_cameraPrefab, _cameraHolder.position, _cameraHolder.rotation, _cameraHolder);
+            if (!IsOwner || _cameraHolder == null)
+                return;
+
+            if (_activeCamera == null)
+                _activeCamera = Camera.main;
+
+            if (_activeCamera == null)
+            {
+                GameObject cameraObject = new GameObject("Player Camera");
+                _activeCamera = cameraObject.AddComponent<Camera>();
+                cameraObject.AddComponent<AudioListener>();
+            }
+
+            if (_activeCamera != null)
+            {
+                _activeCamera.gameObject.SetActive(true);
+                _activeCamera.enabled = true;
+                _activeCamera.tag = "MainCamera";
+                _activeCamera.transform.SetParent(_cameraHolder, false);
+                _activeCamera.transform.localPosition = new Vector3(0f, 0f, -10f);
+                _activeCamera.transform.localRotation = Quaternion.identity;
+                _activeCamera.clearFlags = CameraClearFlags.SolidColor;
+                _activeCamera.backgroundColor = _backgroundColor;
+                _activeCamera.orthographic = true;
+                _activeCamera.orthographicSize = _orthographicSize;
+                _activeCamera.depth = -1;
+                _activeCamera.allowHDR = false;
+                _activeCamera.allowMSAA = false;
+                _activeCamera.cullingMask = -1;
+            }
         }
     }
 #else
-    public sealed class FishnetPlayerCamera : MonoBehaviour
+    public sealed class FishNetCharacterCamera : MonoBehaviour
     {
     }
 #endif
